500a176756567b8b9ab225a2dbb2f692f146ab25
Manashjyoti Barman <manashjyoti.barman07@gmail.com>
2026-09-03 12:12:49 +0530

    Revert static export config to support native Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: isGithubActions ? '/github_profile' : '',
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
